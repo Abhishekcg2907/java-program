03d1eb33b104055f02d6d6c90385a077d4fc7352
Abhishekcg2907 <132828731+Abhishekcg2907@users.noreply.github.com>
2023-05-07 19:42:11 +0530

    Add files via upload

---
+++ collection.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Collection
+{
+    public static void main(String arg[])
+    {
+        LinkedList <Integer> lobj = new LinkdList<Integer>();
+
+        lobj.add(11);
+        lobj.add(21);
+        lobj.add(51);
+        lobj.add(101);
+
+        System.out.println("Elemnets are :"+lobj);
+
+        Iterator lobj = lobj.iterator();
+        while(iobj.hasNext())
+        {
+
+            System.out.println(lobj.Next());
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+}
